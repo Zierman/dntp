8ffe9a843a4bc48a38aa43d1531e201baccdff18
dw <davidwhitebird@gmail.com>
2018-09-19 03:18:41 -0500

    Initial

---
+++ src/socketpractice/ClientSideSocket.java
@@ -0,0 +1,43 @@
+package socketpractice;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ClientSideSocket {
+
+    public static void main(String[] args) {
+        String hostname = args.length > 0 ? args[0] : "http://www.time.nist.gov";
+
+        // Note just completely changing the value of hostname here
+        hostname = "127.0.0.1";
+        Socket socket = null;
+
+        System.out.println("Client main is running");
+
+        try {
+            System.out.println("Attempting try block");
+            socket = new Socket(hostname, 1333);
+            System.out.println("client has connected to "+ socket.getRemoteSocketAddress());
+            socket.setSoTimeout(15000);
+            InputStream in = socket.getInputStream();
+            StringBuilder time = new StringBuilder();
+            InputStreamReader reader = new InputStreamReader(in, "ASCII");
+            for (int c = reader.read(); c != -1; c = reader.read()) time.append((char) c);
+                System.out.println(time);
+
+        } catch (IOException ex) {
+            System.err.println(ex);
+        } finally {
+            if (socket != null) {
+                try { socket.close();
+                } catch (IOException ex) { }
+            }
+        }
+    }
+
+
+
+
+}
---
+++ src/socketpractice/ServerSideSocket.java
@@ -0,0 +1,33 @@
+package socketpractice;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class ServerSideSocket {
+
+    public final static int PORT = 1333;
+
+    public static void main (String[] args) {
+        System.out.println("The server's main is running");
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            while (true) {
+                try (Socket connection = server.accept()) { //accept will return a Socket representing the connection
+                    System.out.println("server try loop after accept running");
+                    Writer out = new OutputStreamWriter(connection.getOutputStream());
+                    Date now = new Date();
+                    out.write(now.toString() + " here is the string written out"+"/r/n");
+                    out.flush();
+                    connection.close();
+                } catch (IOException ex) {
+                }
+            }
+        } catch (IOException ex) {
+            System.err.println((ex));
+            System.out.println("server hit the 2nd error catch");
+        }
+    }
+}
