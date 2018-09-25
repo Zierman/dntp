02b14a35f951875c3c21d65014223c47c599e73a
dw <davidwhitebird@gmail.com>
2018-09-25 17:45:48 -0500

    save

---
+++ src/project1/UdpServer.java
@@ -0,0 +1,13 @@
+package project1;
+
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+
+
+public class UdpServer{
+//    public final SOCKET_NUMBER = 3555
+//
+//    socket = new DatagramSocket(SOCKET_NUMBER);
+
+
+}
