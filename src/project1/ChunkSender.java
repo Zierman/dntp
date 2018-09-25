bad1879564a8fd4f30632a949cd4f2f45cfed8d4
Joshua Zierman <py1422xs@metrostate.edu>
2018-09-25 16:13:20 -0500

    I got board and wrote up some of ChunkSender. It is not done though.

---
+++ src/project1/ChunkSender.java
@@ -0,0 +1,93 @@
+/**
+ * File Created by Joshua Zierman on Sep 25, 2018
+ */
+package project1;
+
+import java.net.InetAddress;
+import java.util.LinkedList;
+
+/**
+ * @author Joshua Zierman [py1422xs@metrostate.edu]
+ *
+ */
+public class ChunkSender implements Sender
+{
+	private InetAddress destinationIp;
+	private int destinationPort;
+	private Chunk[] chunks;
+	private int next = 0; // index of next
+	
+	
+	/** Constructs a ChunkSender
+	 * @param ip InetAddress of destination
+	 * @param port
+	 */
+	public ChunkSender(InetAddress ip, int port)
+	{
+		setToAddress(ip);
+		setToPort(port);
+	}
+	
+	/** loads the sender with Chunks
+	 * @param chunks the array of Chunks
+	 */
+	public void load(Chunk[] chunks)
+	{
+		this.chunks = chunks;
+	}
+	
+	/* (non-Javadoc)
+	 * @see project1.Sender#setToAddress(java.net.InetAddress)
+	 */
+	@Override
+	public void setToAddress(InetAddress ip)
+	{
+		destinationIp = ip;
+	}
+
+	/* (non-Javadoc)
+	 * @see project1.Sender#setToPort(int)
+	 */
+	@Override
+	public void setToPort(int port)
+	{
+		destinationPort = port;
+	}
+
+	/* (non-Javadoc)
+	 * @see project1.Sender#sendNext()
+	 */
+	@Override
+	public void sendNext()
+	{
+		Chunk c = chunks[next++];
+		sendChunk(c);
+	}
+
+	/** sends a Chunk
+	 * @param c Chunk to send
+	 */
+	private void sendChunk(Chunk c)
+	{
+		// TODO this method needs to be completed
+	}
+
+	/* (non-Javadoc)
+	 * @see project1.Sender#resetNextPosition()
+	 */
+	@Override
+	public void resetNextPosition()
+	{
+		next = 0;		
+	}
+
+	/* (non-Javadoc)
+	 * @see project1.Sender#hasNext()
+	 */
+	@Override
+	public boolean hasNext()
+	{
+		return next < chunks.length;
+	}
+
+}
---
+++ src/project1/Sender.java
@@ -0,0 +1,38 @@
+/**
+ * File Created by Joshua Zierman on Sep 25, 2018
+ */
+package project1;
+
+import java.net.InetAddress;
+
+/**
+ * @author Joshua Zierman [py1422xs@metrostate.edu]
+ *
+ */
+public interface Sender
+{
+	/** sets the sender's destination address 
+	 * @param ip the InetAddress of the destination
+	 */
+	public abstract void setToAddress(InetAddress ip);
+	
+	/** sets the destination port
+	 * @param port the integer of the destination port
+	 */
+	public abstract void setToPort(int port);
+	
+	/** sends the next datagram
+	 * 
+	 */
+	public abstract void sendNext();
+	
+	/** sets the index of next to the start
+	 * 
+	 */
+	public abstract void resetNextPosition();
+	
+	/** checks if there is a next value to send
+	 * @return true if sendNext will not be out of bounds, else false
+	 */
+	public abstract boolean hasNext();
+}
