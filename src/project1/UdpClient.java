76dc54c5966901c8fb83f8b814d1da0e096d8331
Joshua Zierman <py1422xs@metrostate.edu>
2018-10-01 05:04:41 -0500

    Pulling constants from Project1 class

--- src/project1/UdpClient.java
+++ src/project1/UdpClient.java
@@ -12,8 +12,8 @@ public class UdpClient{
 
 
     public static void main(String[] args) throws IOException {
-        final int SOCKET_NUMBER = 3555;
-        final String FILE_TO_SEND = "";
+        final int SOCKET_NUMBER = Project1.getPort();//was 3555
+        final String FILE_TO_SEND = Project1.getInputFilename();//was ""
 
         System.out.print("udp client running");
 
