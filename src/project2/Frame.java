0c663939d5108ec61cb48944cfe851d0a60a81ff
Joshua Zierman <py1422xs@metrostate.edu>
2018-10-02 22:45:32 -0500

    Started Frame stuff

---
+++ src/project2/Frame.java
@@ -0,0 +1,48 @@
+/**
+ * File Created by Joshua Zierman on Oct 2, 2018
+ */
+package project2;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.util.LinkedList;
+
+/**
+ * @author Joshua Zierman [py1422xs@metrostate.edu]
+ *
+ */
+public abstract class Frame
+{
+	protected final Byte FLAG = 0x7E;
+	protected final Byte ESC = 0x7D;
+	public enum Error {DROP, DELAY, CORRUPT}
+	protected Error error = null;
+	protected Byte frameNumber;
+	
+	protected Frame(byte frameNumber)
+	{
+		this.frameNumber = frameNumber;
+	}
+	
+	public Frame()
+	{
+		
+	}
+	
+	public abstract DatagramPacket toDatagramPacket(InetAddress address, int port);
+
+	public Byte getFrameNumber()
+	{
+		return frameNumber;
+	}
+	
+	public Error getError()
+	{
+		return error;
+	}
+	
+	private static Byte toggle(Byte b)
+	{
+		return (byte) (b ^ 0x20);
+	}
+}
---
+++ src/project2/Project2.java
@@ -0,0 +1,13 @@
+/**
+ * File Created by Joshua Zierman on Oct 2, 2018
+ */
+package project2;
+
+/**
+ * @author Joshua Zierman [py1422xs@metrostate.edu]
+ *
+ */
+public class Project2
+{
+	final int TIMEOUT = 2000; // 2000ms
+}
