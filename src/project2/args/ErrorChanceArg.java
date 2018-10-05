3c25ced6d810b47c038467d73cddd14982938846
dw <davidwhitebird@gmail.com>
2018-10-05 13:11:41 -0500

    ErrorIntroduce class created

--- src/project2/args/ErrorChanceArg.java
+++ src/project2/args/ErrorChanceArg.java
@@ -2,26 +2,31 @@ package project2.args;
 
 import project2.Defaluts;
 
-public class ErrorChanceArg extends Arg <Integer> {
+public class ErrorChanceArg extends Arg <Integer>
+{
     private final static String HELP_MSG = "Percent Chance of an error being generated.";
 
-    public ErrorChanceArg (String flag){
+    public ErrorChanceArg (String flag)
+    {
         super(flag);
     }
 
     @Override
-    protected void processInlineArg(String s) throws Exception {
+    protected void processInlineArg(String s) throws Exception
+    {
         value = Integer.parseInt(s);
     }
 
     @Override
-    protected Integer getDefault() {
+    protected Integer getDefault()
+    {
 
         return Defaluts.CHANCE_OF_ERROR;
     }
 
     @Override
-    protected String getHelpString() {
+    protected String getHelpString()
+    {
         return HELP_MSG;
     }
 }
