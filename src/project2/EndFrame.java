74b4ede3dd10037dc6b5e67c3e00ed3df775663b
Joshua Zierman <py1422xs@metrostate.edu>
2018-11-05 00:32:42 -0600

    Created a EndFrame class that extends ChunkFrame.

---
+++ src/project2/EndFrame.java
@@ -0,0 +1,20 @@
+package project2;
+
+import project1.Chunk;
+import project2.frame.ChunkFrame;
+
+/** A special ChunkFrame that contains no data that is used to flag end of transmission
+ * @author Joshua Zierman [py1422xs@metrostate.edu]
+ *
+ */
+public class EndFrame extends ChunkFrame
+{
+	/** Creates a new EndFrame with the specified sequenceNumber
+	 * @param sequenceNumber the integer sequence number of the ChunkFrame
+	 */
+	public EndFrame(int sequenceNumber)
+	{
+		super(new Chunk(new byte[0], 0), sequenceNumber);
+	}
+
+}
